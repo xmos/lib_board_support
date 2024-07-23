53164671df9bea48122b0dfdd94d5f9f0d71f7c7
Huw Percival <107470836+xhuw@users.noreply.github.com>
2024-07-23 08:31:16 +0100

    Update lib_build_info.cmake

--- lib_board_support/lib_build_info.cmake
+++ lib_board_support/lib_build_info.cmake
@@ -2,6 +2,6 @@ set(LIB_NAME lib_board_support)
 set(LIB_VERSION 0.1.0)
 set(LIB_INCLUDES api/boards api/drivers)
 set(LIB_COMPILER_FLAGS -O3 -g)
-set(LIB_DEPENDENT_MODULES "lib_i2c(>=6.2.0)")
+set(LIB_DEPENDENT_MODULES "lib_i2c(6.2.0)")
 
 XMOS_REGISTER_MODULE()
