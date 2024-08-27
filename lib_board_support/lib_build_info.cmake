7abc5cd088d53043d90117de06cfab36986e957a
Ed <ed@xmos.com>
2024-08-27 16:07:27 +0100

    Missing dep for xcommon and change optimisation to -Os as non-timing critical code

--- lib_board_support/lib_build_info.cmake
+++ lib_board_support/lib_build_info.cmake
@@ -1,7 +1,7 @@
 set(LIB_NAME lib_board_support)
 set(LIB_VERSION 0.1.0)
 set(LIB_INCLUDES api/boards api/drivers)
-set(LIB_COMPILER_FLAGS -O3 -g)
+set(LIB_COMPILER_FLAGS -Os -g)
 set(LIB_DEPENDENT_MODULES   "lib_i2c(6.2.0)"
                             "lib_sw_pll(2.2.0)")
 
