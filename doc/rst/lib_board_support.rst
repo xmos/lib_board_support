2064117f49077d77bf2d14b33b5f1e61c42cde25
Ed <ed@xmos.com>
2024-08-29 17:38:14 +0100

    Initial doxy comments for 316_mc

--- doc/rst/lib_board_support.rst
+++ doc/rst/lib_board_support.rst
@@ -17,7 +17,7 @@ development kits. The API is broken down into 2 sections:
 Usage
 *****
 
-This repo supports XCommon CMake. Simply add "lib_board_support" to an applications "APP_DEPENDENT_MODULES". The application
+This repo supports XCommon CMake. Simply add `lib_board_support` to an applications cmake file using the `APP_DEPENDENT_MODULES` entry. The application
 must provide the xn file. The application must use the APIs for the specific board that it is using. To ensure that only the correct sources for the board in use get compiled in, it is necessary to set the preprocessor value `BOARD_SUPPORT_BOARD` to one of the available boards listed in `api/boards/boards_utils.h`. This can be done in the app with the following snippet of cmake::
 
     set(APP_COMPILER_FLAGS
@@ -25,26 +25,28 @@ must provide the xn file. The application must use the APIs for the specific boa
 	)
 
 
-From the application it is necessary to include the relevant header file. For example::
+From the application where board initialisation of configuration is done it is necessary to include the relevant header file. For example::
 
     #include "xk_audio_316_mc_ab/board.h"
 
 From then onwards in your code you may call the relevant API functions to setup and configure the board hardware.
 
+Note that in some cases, the XCORE tile that calls the configuration function (usually from I2S initilaisation) is different from the tile where I2C master is placed. Since I2C master is required by most audio CODECs for configuration and tiles can only communicate via channels, a remote server is needed to provide the I2C setup. This usually takes the form of a task which is run on a thread placed on the I2C tile and is controlled via a channel. The cross-tile channel must be delcared at the top-level XC main function. The included examples show examples of this using both XC and C.
+
 ****************
 Supported Boards
 ****************
 
-The following boards are supported in this repo with interfaces provided in the languages shown in the table.
+The following boards are supported in this repo with interfaces provided in the languages shown in the table below.
 
 +--------------------+---------------------+
 | Board              | Supported Languages |
 +====================+=====================+
-|XK_EVK_XU316        | XC                  |
+|XK_EVK_XU316        | XC / C              |
 +--------------------+---------------------+
 |XK_AUDIO_316_MC_AB  | XC                  |
 +--------------------+---------------------+
-|XK_AUDIO_216_MC_AB  | XC                  |
+|XK_AUDIO_216_MC_AB  | XC / C              |
 +--------------------+---------------------+
 
 Common API
--- lib_board_support/api/boards/xk_audio_316_mc_ab/board.h
+++ lib_board_support/api/boards/xk_audio_316_mc_ab/board.h
@@ -10,7 +10,7 @@
  * @{
  */
 
-#if __XC__
+#if defined(__XC__) || defined(__DOXYGEN__)
 
 #include "i2c.h"
 
@@ -19,43 +19,91 @@ extern port p_scl;
 extern port p_sda;
 
 
-/// Start an i2s master thread which uses the DAC pins
-#define xk_audio_316_mc_ab_i2c_master(i2c) i2c_master((i2c), 1, p_scl, p_sda, 100)
+/** This define is a specialised alias of the I2C master task and provides the remote
+ * I2C master running on a different tile. For this hardware this task needs to be run
+ * on tile[0].
+ */
+ #define xk_audio_316_mc_ab_i2c_master(i2c) i2c_master((i2c), 1, p_scl, p_sda, 100)
 
-
+/** Type of clock to be instantiated. This may be a fixed clock using the application PLL,
+ * an adjustable clock using the CS2100 external PLL or and adjustable clock using application
+ * PLL.
+ */
 typedef enum {
     CLK_FIXED,
     CLK_CS2100,
     CLK_PLL
 } xk_audio_316_mc_ab_mclk_modes_t;
 
+/** Formats supported by the DAC and ADC. Either I2S using multiple data lines or TDM
+ * supporting multi-channel using a single data line.
+ */
 typedef enum {
     AUD_316_PCM_FORMAT_I2S,
     AUD_316_PCM_FORMAT_TDM
 } xk_audio_316_mc_ab_pcm_format_t;
 
+/** @struct xk_audio_316_mc_ab_config_t
+ *  @brief Configuration struct type for setting the hardware profile.
+ *  @var xk_audio_316_mc_ab_config_t::clk_mode
+ *  See xk_audio_316_mc_ab_mclk_modes_t for available options.
+ *  @var xk_audio_316_mc_ab_config_t::dac_is_clock_master
+ *  Boolean setting for whether the DAC or the xcore.ai is I2S clock master. Set to 0 to make the xcore.ai master.
+ *  @var xk_audio_316_mc_ab_config_t::default_mclk
+ *  Nominal clock frequency in MHz. Standard rates are supported between 11.2896 MHz and 49.152 MHz.
+ *  @var xk_audio_316_mc_ab_config_t::pll_sync_freq
+ *  When the CLK_CS2100 is used, this defines the nominal reference clock frequency for multiplication by the PLL.
+ *  This value is ignored when the CS2100 is not used.
+ *  *  @var xk_audio_316_mc_ab_config_t::pcm_format
+ *  See xk_audio_316_mc_ab_pcm_format_t for available data frame options.
+ *  @var xk_audio_316_mc_ab_config_t::i2s_n_bits
+ *  Number of bits per data frame in I2S.
+ *  @var xk_audio_316_mc_ab_config_t::i2s_chans_per_frame
+ *  This defines the number of audio channels per frame (a frame is a complete cycle of FSYNC or LRCLK).
+ * 
+ */
 typedef struct {
     xk_audio_316_mc_ab_mclk_modes_t clk_mode;
     char dac_is_clock_master; // bool
-
-    // fixed clock config
     unsigned default_mclk;
-
-    // cs2100 clock config
     unsigned pll_sync_freq;
-
     xk_audio_316_mc_ab_pcm_format_t pcm_format;
     unsigned i2s_n_bits;
-
     unsigned i2s_chans_per_frame;
 } xk_audio_316_mc_ab_config_t;
 
+/** Performs the required port operations to enable and the audio hardware on the platform. Must be called from tile[0]
+ * and *before* xk_audio_316_mc_ab_AudioHwInit() is called.
+ *
+ *  \param   config     Reference to the xk_audio_316_mc_ab_config_t configuration struct.
+ */
 void xk_audio_316_mc_ab_board_setup(const xk_audio_316_mc_ab_config_t &config);
 
+/** Initialises the audio hardware ready for a configuration. Must be called once *after* xk_audio_316_mc_ab_board_setup().
+ *
+ *  \param   i2c        client side of I2C master interface connection.
+ *  \param   config     Reference to the xk_audio_316_mc_ab_config_t configuration struct.
+ */
 void xk_audio_316_mc_ab_AudioHwInit(client interface i2c_master_if i2c, const xk_audio_316_mc_ab_config_t& config);
 
 
-void xk_audio_316_mc_ab_AudioHwConfig(client interface i2c_master_if i2c, const xk_audio_316_mc_ab_config_t& config, unsigned samFreq, unsigned mClk, unsigned dsdMode, unsigned sampRes_DAC, unsigned sampRes_ADC);
+/** Configures the audio hardware following initialisation. This is typically called each time a sample rate or stream format change occurs.
+ *
+ *  \param   i2c            client side of I2C master interface connection.
+ *  \param   config         Reference to the xk_audio_316_mc_ab_config_t configuration struct.
+ *  \param   samFreq        The sample rate in Hertz.
+ *  \param   mClk           The master clock rate in Hertz.
+ *  \param   dsdMode        Controls whether the DAC is to be set into DSD mode (1) or PCM mode (0).
+ *  \param   sampRes_DAC    The sample resolution of the DAC output in bits. Typically 16, 24 or 32.
+ *  \param   sampRes_ADC    The sample resolution of the ADC input in bits. Typically 16, 24 or 32.
+ */
+void xk_audio_316_mc_ab_AudioHwConfig(  client interface i2c_master_if i2c,
+                                        const xk_audio_316_mc_ab_config_t& config,
+                                        unsigned samFreq,
+                                        unsigned mClk,
+                                        unsigned dsdMode,
+                                        unsigned sampRes_DAC,
+                                        unsigned sampRes_ADC);
 
 #endif
 
