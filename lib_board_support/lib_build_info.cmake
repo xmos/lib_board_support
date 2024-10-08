9ee32e789baa75b01904480e11e42bb3ffd8bba3
Ed <ed@xmos.com>
2024-10-08 13:47:30 +0100

    Tidy cmake and bump lib_bsp

--- lib_board_support/lib_build_info.cmake
+++ lib_board_support/lib_build_info.cmake
@@ -3,6 +3,6 @@ set(LIB_VERSION 1.1.0)
 set(LIB_INCLUDES api/boards api/drivers)
 set(LIB_COMPILER_FLAGS -Os -g)
 set(LIB_DEPENDENT_MODULES   "lib_i2c(6.2.0)"
-                            "lib_sw_pll(2.2.0)")
+                            "lib_sw_pll(2.3.0)")
 
 XMOS_REGISTER_MODULE()
