60273f5aac77dabaacccc682f434a085e33c3548
Humphrey Bucknell <humphreybucknell@xmos.com>
2025-08-27 11:44:14 +0100

    Adding baord names to defines

--- lib_board_support/api/boards/boards_utils.h
+++ lib_board_support/api/boards/boards_utils.h
@@ -20,19 +20,19 @@
 /** Define representing Null board i.e. no board in use*/
 #define NULL_BOARD                  0
 
-/** Define representing XK-AUDIO-216-MC Board */
+/** Define representing XK-AUDIO-216-MC, xcore-200 Multi-channel AudioBoard */
 #define XK_AUDIO_216_MC_AB          1
 
-/** Define representing XK-AUDIO-316-MC Board */
+/** Define representing XK-AUDIO-316-MC, xcore.ai Multi-channel Audio Board */
 #define XK_AUDIO_316_MC_AB          2
 
-/** Define representing XK-EVK-XU316 board */
+/** Define representing XK-EVK-XU316, xcore.ai Explorer Evaluation Kit board */
 #define XK_EVK_XU316                3
 
-/** Define representing XK-EVK-XU216 board */
+/** Define representing XK-EVK-XU216, xcore-200 Explorer Evaluation Kit board */
 #define XK_EVK_XE216                4
 
-/** Define representing XK-ETH-316-DUAL board */
+/** Define representing XK-ETH-316-DUAL, xcore.ai Ethernet Development Kit board */
 #define XK_ETH_316_DUAL             5
   
 /** Total number of boards supported by the library */
